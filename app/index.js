8facd228e39b249fd81cac4f8742f11265e66fc2
andrewangelle <andrewangelle@gmail.com>
2017-06-18 15:15:19 -0500

    updated package.json name

--- app/index.js
+++ app/index.js
@@ -25,9 +25,7 @@ module.exports = yeoman.generators.Base.extend({
       this.template('gitignore', '.gitignore');
       this.template('eslintrc', '.eslintrc');
       this.template('babelrc', '.babelrc');
-      this.template('jestrc', '.jestrc');
       this.template('webpack.config.js');
-      this.template('webpack.config.production.js');
       this.template('_package.json', 'package.json');
       this.template('README.md');
       this.directory('build', 'build');
