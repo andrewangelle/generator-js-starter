b93faf266580f7adcc2c02c4b2b349e793d819d6
Zak Angelle <zaknotzak@gmail.com>
2017-06-18 15:12:21 -0500

    Update generator script to reflect new Yeoman API (#1)

--- app/index.js
+++ app/index.js
@@ -1,43 +1,66 @@
+'use strict';
+const path = require('path');
+const Generator = require('yeoman-generator');
 
-const yeoman = require('yeoman-generator');
-const _s = require('underscore.string');
+module.exports = class extends Generator {
+  initializing() {
+    this.props = {};
+  }
 
-module.exports = yeoman.generators.Base.extend({
-  init: function() {
-    var cb = this.async();
-
-    this.prompt([
+  prompting() {
+    return this.prompt([
       {
-        name: 'appName',
-        message: 'App name',
-        default: this.appname.replace(/\s/g, '-'),
-        filter: val => _s.slugify(val)
+        type    : 'input',
+        name    : 'appName',
+        message : 'Your project name',
+        default : this.appname
       },
       {
-        name: 'appDescription',
-        message: 'App description'
+        type    : 'input',
+        name    : 'appDescription',
+        message : 'Your project description'
       }
-    ],
-    props => {
-      this.appName = props.appName;
-      this.appDescription = props.appDescription;
-
-      this.template('gitignore', '.gitignore');
-      this.template('eslintrc', '.eslintrc');
-      this.template('babelrc', '.babelrc');
-      this.template('jestrc', '.jestrc');
-      this.template('webpack.config.js');
-      this.template('webpack.config.production.js');
-      this.template('_package.json', 'package.json');
-      this.template('README.md');
-      this.directory('build', 'build');
-      this.directory('src', 'src');
-      this.directory('test', 'test');
-
-      cb();
+    ])
+    .then(answers => {
+      this.props.appName = answers.appName;
+      this.props.appDescription = answers.appDescription;
     });
-  },
-  install: function() {
-    this.installDependencies({ bower: false });
   }
-});
+
+  writing() {
+    this.fs.copy(
+      this.templatePath('src'),
+      this.destinationPath('src')
+    );
+
+    this.fs.copy(
+      this.templatePath('.babelrc'),
+      this.destinationPath('.babelrc')
+    );
+
+    this.fs.copy(
+      this.templatePath('.eslintrc'),
+      this.destinationPath('.eslintrc')
+    );
+
+    this.fs.copy(
+      this.templatePath('.gitignore'),
+      this.destinationPath('.gitignore')
+    );
+
+    this.fs.copy(
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js')
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json'),
+      this.props
+    );
+  }
+
+  install() {
+    this.spawnCommand('npm', ['install']);
+  }
+};
